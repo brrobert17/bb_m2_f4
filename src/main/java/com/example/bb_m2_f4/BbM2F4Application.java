387a544cd1b182967c2616c9f5b99cb8958bb03a
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2022-04-26 13:05:25 +0200

    Initial commit

---
+++ src/main/java/com/example/bb_m2_f4/BbM2F4Application.java
@@ -0,0 +1,13 @@
+package com.example.bb_m2_f4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbM2F4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbM2F4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bb_m2_f4/BbM2F4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bb_m2_f4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbM2F4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
